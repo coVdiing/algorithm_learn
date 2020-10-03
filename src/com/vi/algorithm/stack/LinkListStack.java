f7ff36bf6f46b82af6944f81b7d0078ee27fd2b0
coVdiing <113714806@qq.com>
2020-10-04 07:42:30 +0800

    用链表实现栈

---
+++ src/com/vi/algorithm/stack/LinkListStack.java
@@ -0,0 +1,52 @@
+package com.vi.algorithm.stack;
+
+import com.vi.algorithm.linear.LinkList;
+
+//  用链表实现栈
+public class LinkListStack {
+    private LinkList<Integer> stack;
+
+    public LinkListStack() {
+        stack = new LinkList<Integer>();
+    }
+
+    //  链表式的不存在存满的情况
+    public boolean isFull() {
+        return false;
+    }
+
+    //  判断是否为空
+    public boolean isEmpty() {
+        return stack.length() == 0;
+    }
+
+    public Integer pop() {
+        if (stack.isEmpty()) {
+            System.out.println("栈为空！");
+            return null;
+        }
+        Integer result = stack.remove(stack.length() - 1);
+        return result;
+    }
+
+    public boolean push(Integer data) {
+        try {
+            stack.insert(data);
+        } catch (Exception e) {
+            System.out.println("插入失败！");
+            return false;
+        }
+        return true;
+    }
+
+    //  遍历
+    public void list() {
+        if (stack.isEmpty()) {
+            System.out.println("栈为空！");
+            return;
+        }
+        for (int i = stack.length() - 1; i >= 0; i--) {
+            System.out.printf("stack[%d] = %d\n",i,stack.get(i));
+        }
+    }
+}
